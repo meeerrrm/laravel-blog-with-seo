7b86417e2222bbef6fb8b33770aef761e7c500f5
Mohammad Entol Rizky <mohammad.erm08@gmail.com>
2023-04-16 02:09:40 +0700

    Component for back & update primary

---
+++ BLOGSYS/resources/views/components/a-button-back.blade.php
@@ -0,0 +1,1 @@
+<a {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-gray-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-grey-700 focus:bg-grey-700 active:bg-grey-700 focus:outline-none focus:ring-2 focus:ring-grey-700 focus:ring-offset-2 transition ease-in-out duration-150 mr-2']) }}>{{ $slot }}</a>
