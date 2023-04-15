9bcd0c056ae1e3950c21e00414ec7b5a53f8724d
Mohammad Entol Rizky <mohammad.erm08@gmail.com>
2023-04-16 01:18:10 +0700

    update prefix & vite.config

--- BLOGSYS/vite.config.js
+++ BLOGSYS/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
     plugins: [
@@ -9,7 +10,10 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
-            refresh: true,
+            refresh: false,
         }),
+        tailwindcss({
+            refresh: true,
+        })
     ],
 });
