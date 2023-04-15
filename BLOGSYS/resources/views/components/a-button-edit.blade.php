862bb045116d2ae790407836063dd263f79bff23
Mohammad Entol Rizky <mohammad.erm08@gmail.com>
2023-04-16 01:19:25 +0700

    Add components

---
+++ BLOGSYS/resources/views/components/a-button-edit.blade.php
@@ -0,0 +1,1 @@
+<a {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-yellow-400 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-500 focus:bg-yellow-500 active:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>{{ $slot }}</a>
