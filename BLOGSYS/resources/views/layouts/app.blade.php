77a3d33b4dc2fb5fd466184d684323e74c6eeb66
Mohammad Entol Rizky <mohammad.erm08@gmail.com>
2023-04-22 04:22:20 +0700

    Change config title

--- BLOGSYS/resources/views/layouts/app.blade.php
+++ BLOGSYS/resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Entol Rizky') }}</title>
+        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/logo/logo.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
