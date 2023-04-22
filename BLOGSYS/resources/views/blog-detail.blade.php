108c92ac5f69dcf30b43eb2c18c03b6d0bc71e0c
Mohammad Entol Rizky <mohammad.erm08@gmail.com>
2023-04-23 00:27:59 +0700

    Update: add Content Author

--- BLOGSYS/resources/views/blog-detail.blade.php
+++ BLOGSYS/resources/views/blog-detail.blade.php
@@ -16,10 +16,26 @@
                 <div class="text-left text-slate-300 mt-8" id="mainContent">
 {!! json_decode($blog->content) !!}
                 </div>
+                <div class="text-slate-300 mt-24">
+                    <div class="grid grid-cols-1 md:grid-cols-2">
+                        <div class="flex flex-wrap">
+                            <div class="w-1/3">
+                                <div class="rounded-full w-14 h-14 overflow-hidden mx-auto">
+                                    <img src="{{ asset('assets/picture/'.$blog->user->profile_picture) }}" class="h-14">
+                                </div>
+                            </div>
+                            <div class="w-2/3 text-left">
+                                <h3 class="text-slate-100 font-bold">{{ strtoupper($blog->user->name) }}</h3>
+                                <p>Publish at <b>{{ date('d F Y',strtotime($blog->publish_date)) }}</b></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     <x-slot name="js">
         <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+<!-- place for config your content css -->
         <script>
             $(document).ready(function(){	
                 $('#mainContent').closest('div').find('ol').addClass('list-decimal ml-4 mt-2');
