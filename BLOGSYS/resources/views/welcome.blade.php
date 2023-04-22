23e742925bb1c99a70436971566a832950f80092
Mohammad Entol Rizky <mohammad.erm08@gmail.com>
2023-04-23 00:27:40 +0700

    Update: delete outline at blog list card

--- BLOGSYS/resources/views/welcome.blade.php
+++ BLOGSYS/resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
 @foreach($blogs as $blog)
 					<div>
 						<a href="{{ route('blog.detail',$blog->uniq) }}" class="h-full w-full">
-							<div class="h-full group shadow-lg hover:scale-110  rounded-xl overflow-hidden outline outline-2 outline-sky-600 -outline-offset-2 transition-all">
+							<div class="h-full group shadow-lg hover:scale-110  rounded-xl overflow-hidden transition-all">
 								<div class="h-44 overflow-hidden bg-slate-700">
 									<img src="{{ asset('assets/blog/'.$blog->thumnail) }}" class="h-44 w-auto transition-all object-contain mx-auto" alt="{{ $blog->title }}">
 								</div>
