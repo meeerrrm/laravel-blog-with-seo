5ef3232068f97c4fe8faeaed7af4783bddfd8b26
Mohammad Entol Rizky <mohammad.erm08@gmail.com>
2023-05-27 10:34:12 +0700

    Develop: Add Style & Script to guest.blade.php

--- BLOGSYS/resources/views/layouts/guest.blade.php
+++ BLOGSYS/resources/views/layouts/guest.blade.php
@@ -10,9 +10,15 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+@if($_SERVER['REMOTE_ADDR'] === "127.0.0.1")
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+@else
+<!-- CSS -->
+        <link rel="stylesheet" href="{{ asset('build/assets/main.css') }}">
+<!-- Scripts -->
+        <link rel="stylesheet" href="{{ asset('build/assets/app-919ba201.js') }}">
+        <link rel="stylesheet" href="{{ asset('build/assets/main.js') }}">
+@endif
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
